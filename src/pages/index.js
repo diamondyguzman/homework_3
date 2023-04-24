146df48dffbe9a3e51cbcdbb8ea8fc3d4adb46ba
dlllooo <dyguzman@iu.edu>
2023-04-24 17:21:21 -0400

    firebase depency

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function HomePage(){
   return(
     <>
-      <Component {...pageProps}/>
+      <h1>My name :{process.env.NEXT_PUBLIC_MY_NAME}</h1>
+   
     </>
   )
 }
